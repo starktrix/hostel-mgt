4d2419d7eb7684d8d5cc8ccbfa8d4391dcaa7359
starktrix <82788294+starktrix@users.noreply.github.com>
2025-12-27 08:45:58 +0000

    code overall changes and improvement

--- server/data/students.js
+++
@@ -1,2 +0,0 @@
-const students = [];
-export default students;
--- server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateToken = (id) => {  
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '2h',
-  })
-}
-
-export default generateToken
